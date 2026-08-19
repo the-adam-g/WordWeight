ca03b2bbd392379101eb93d8bd6d2c9b3f6f0e52
Adam Gillion <adam@queuedesk.org>
2026-08-19 14:53:47 +0100

    Update version date in main.cs

--- Versions/V2.1/main.cs
+++ Versions/V2.1/main.cs
@@ -44,7 +44,7 @@
 
 char[] characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '];
 
-Console.WriteLine("---------------------------------------------------\nWordWeight V2.1 (18-08-2026) - Made by Adam Gillion\n---------------------------------------------------");
+Console.WriteLine("---------------------------------------------------\nWordWeight V2.1 (19-08-2026) - Made by Adam Gillion\n---------------------------------------------------");
 
 Console.WriteLine("Enter the first string: ");
 string firstString = Console.ReadLine() ?? "";
